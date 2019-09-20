d6570daf1825a0643b10eb930912d149b800ed90
Jameson831 <jtwhite@mail.csuchico.edu>
2019-09-20 11:43:23 -0700

    Changes to the greetings

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,11 @@
+// Jameson White
 #include <iostream>
 #include <string>
 
 int main() {
+   
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Jameson White. Whats yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
